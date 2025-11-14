11a7cc4af4574e1535995a5059851712cebe3652
Sayaz06 <adha86614@gmail.com>
2025-11-15 01:05:21 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const cacheName = 'nota-terjun-v1';
+const assets = ['.','index.html','manifest.json','icon-192.png','icon-512.png'];
+
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets))); });
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
