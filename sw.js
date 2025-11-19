dcf044b4c0b80ea2fbba27da2a753d31aa7a5c7f
Sayaz06 <adha86614@gmail.com>
2025-11-19 14:18:07 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const cacheName = 'nota-terjun-v2';
+const cacheName = 'nota-terjun-v3';
 const assets = ['.','index.html','manifest.json','IconBrain-192.png','IconBrain-512.png'];
 
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets))); });
 self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+
 
