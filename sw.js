1c3dbe7f1238af693c5387c6db7277789d542236
Sayaz06 <adha86614@gmail.com>
2025-11-19 14:01:34 +0800

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const cacheName = 'nota-terjun-v1';
-const assets = ['.','index.html','manifest.json','icon-192.png','icon-512.png'];
+const cacheName = 'nota-terjun-v2';
+const assets = ['.','index.html','manifest.json','IconBrain-192.png','IconBrain-512.png'];
 
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets))); });
 self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
+
